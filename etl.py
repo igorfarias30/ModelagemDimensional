109165985dc3ba028a39eeb63d77105243913ea8
Igor Farias Sousa <igor_sousa@virgo.atlantico.net.br>
2018-03-05 19:56:00 -0300

    ETL ready

--- etl.py
+++ etl.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 
+# Igor Farias
+# Mateus
+
 def processing_data(data):
     # reading just 100000 rows the dataframe
-    df = pd.read_csv('Data/201701_BolsaFamiliaFolhaPagamento.csv', error_bad_lines=False, encoding = "ISO-8859-1", sep='\t', nrows=100000)
+    df = pd.read_csv('Data/201701_BolsaFamiliaFolhaPagamento.csv', error_bad_lines=False, encoding = "ISO-8859-1", sep='\t', nrows=1000)
 
     df['Regiao'] = None
     # add region name to dataframe
@@ -72,6 +75,8 @@ def processing_data(data):
             return 'Rio Grande do Sul'
         elif uf == 'SC':
             return 'Santa Catarina'
+        elif uf == 'DF':
+            return 'Distrito Federal'
         else:
             return ''
 
@@ -83,40 +88,112 @@ def processing_data(data):
     df['Fonte'] = df['Fonte-Finalidade'].apply(lambda x: x.split('-')[0])
     df['Finalidade'] = df['Fonte-Finalidade'].apply(lambda x: x.split('-')[1])
     df.drop('Fonte-Finalidade', axis=1, inplace = True)
-
     #df.to_csv('Data\\Bolsa_Familia.csv', sep=';')
 
     # return the datasets
     return df
 
-def load_data(dataset):
+# loading data to database
+def load_data_dimension(dataset):
     # Connect to the database
     connection = pymysql.connect(host='localhost', user='root', password='', db='BolsaFamilaDW', charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
 
-    SQL = ''
     try:
         with connection.cursor() as cursor:
-
-            # DimMunicipio
-            for cidade in dataset['Nome Município'].unique():
+            # Adding data to DimMunicipio
+            for cod_siafi in dataset['Código SIAFI Município'].unique():
                 # Create a new record
-                dataset_aux = dataset[dataset['Nome Município'] == cidade]
+                dataset_aux = dataset[dataset['Código SIAFI Município'] == cod_siafi]
 
-                cod_siafi = dataset_aux['Código SIAFI Município'].unique()[0]
+                cidade = dataset_aux['Nome Município'].unique()[0]
                 estado = dataset_aux['Estado'].unique()[0]
                 regiao = dataset_aux['Regiao'].unique()[0]
                 uf = dataset_aux['UF'].unique()[0]
-                sql = """INSERT INTO DimMunicipio (COD_SIAFI, MUNICIPIO, ESTADO, REGIAO, UF) VALUES (%d, "%s", "%s", "%s", "%s")""" %(int(cod_siafi), cidade, estado, regiao, uf) #.format(int(cod_siafi), cidade, estado, regiao, uf)
-                #print(sql)
-                #break
-                SQL = sql
+                sql = """INSERT INTO DimMunicipio (COD_SIAFI, MUNICIPIO, ESTADO, REGIAO, UF) VALUES (%d, "%s", "%s", "%s", "%s")""" %(int(cod_siafi), cidade, estado, regiao, uf)
                 cursor.execute(sql)#, ('webmaster@python.org', 'very-secret'))
+
+            # adding data to DimFavorecido
+            for nis in dataset['NIS Favorecido'].unique():
+                dataset_aux = dataset[dataset['NIS Favorecido'] == nis]
+
+                nome = dataset_aux['Nome Favorecido'].unique()[0]
+                sql = """INSERT INTO DimFavorecido(NIS, NOME) VALUES ("{}", "{}")""".format(nis, nome)
+                cursor.execute(sql)
+
+            # adding data to DimFuncao
+            for funcao in dataset['Código Função'].unique():
+                dataset_aux = dataset[dataset['Código Função'] == funcao]
+
+                subfuncao = dataset_aux['Código Subfunção'].unique()[0]
+                acao = dataset_aux['Código Ação'].unique()[0]
+
+                sql = """INSERT INTO DimFuncao(COD_FUNCAO, COD_SUBFUNCAO, COD_ACAO) VALUES({}, {}, {})""".format(funcao, subfuncao, acao)
+                cursor.execute(sql)
+
+            # add Data to DimFonte
+            for fonte in dataset['Fonte'].unique():
+                dataset_aux = dataset[dataset['Fonte'] == fonte]
+
+                finalidade = dataset_aux['Finalidade'].unique()[0]
+                sql = """INSERT INTO DimFonte(FONTE, FINALIDADE) VALUES("{}", "{}")""".format(fonte, finalidade)
+                cursor.execute(sql)
+
+            #add Data to DimTempo
+            for data in dataset['Mês Competência'].unique():
+                mes, ano = data.split('/')
+                # the data hasn't
+                sql = """INSERT INTO DimTempo(ANO, MES, DIA) VALUES({}, {}, 28)""".format(ano, mes)
+                cursor.execute(sql)
 
         # connection is not autocommit by default. So you must commit to save
         # your changes.
         connection.commit()
-    except:
-        print(SQL)
+    finally:
+        connection.close()
+
+# load data to fact
+def load_data_fact(dataset):
+    # Connect to the database
+    connection = pymysql.connect(host='localhost', user='root', password='', db='BolsaFamilaDW', charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
+
+    try:
+        with connection.cursor() as cursor:
+            # reading all row
+            for i in range(dataset.shape[0]):
+                #idDimMunicipio
+                municipio = """SELECT idDimCidade from DimMunicipio where MUNICIPIO = "{}" AND ESTADO = "{}" LIMIT 1""".format(dataset['Nome Município'][i], dataset['Estado'][i])
+                cursor.execute(municipio)
+                municipio = cursor.fetchone()
+
+                #if
+
+                #idDimTempo
+                mes, ano = df['Mês Competência'][i].split('/')
+                tempo = """SELECT idDimTempo from DimTempo where  MES = {} and ANO = {} LIMIT 1 """.format(int(mes), int(ano))
+                cursor.execute(tempo)
+                tempo = cursor.fetchone()
+
+                #idDimFonte
+                fonte = """SELECT idDimFonte FROM DimFonte where FONTE = "{}" and FINALIDADE = "{}" """.format(dataset['Fonte'][i], dataset['Finalidade'][i])
+                cursor.execute(fonte)
+                fonte = cursor.fetchone()
+
+                #idDimFavorecido
+                favorecido = """SELECT idDimFavorecido FROM DimFavorecido where NIS = '{}' """.format(dataset['NIS Favorecido'][i])
+                cursor.execute(favorecido)
+                favorecido = cursor.fetchone()
+
+                #idDimFuncao
+                funcao = """SELECT idDimFuncao FROM DimFuncao where COD_FUNCAO = {} """.format(dataset['Código Função'][i])
+                cursor.execute(funcao)
+                funcao = cursor.fetchone()
+
+                # loading data to fact table
+                insert = """INSERT INTO FatBolsaFamilia (idDimCidade, idDimFavorecido, idDimFuncao, idDimFonte, idDimTempo, VALOR_PARCELA) VALUES (%d, %d, %d, %d, %d, %.2f)"""%(municipio['idDimCidade'], favorecido['idDimFavorecido'], funcao['idDimFuncao'], fonte['idDimFonte'], tempo['idDimTempo'], dataset['Valor Parcela'][i])
+                cursor.execute(insert)
+
+        # commit data
+        connection.commit()
     finally:
         connection.close()
 
@@ -128,7 +205,7 @@ if __name__ == '__main__':
     try:
         import pymysql.cursors
     except ImportError:
-        print("missing 'pymysql'. Type 'pip install pymysql' to install package")
+        print("missing 'pymysql'. Type 'pip install PyMySQL' to install package")
         exit()
 
     if len(sys.argv) < 1:
@@ -136,4 +213,5 @@ if __name__ == '__main__':
         exit()
 
     df = processing_data("Data/{}".format(sys.argv[1]))
-    load_data(df)
+    load_data_dimension(df)
+    load_data_fact(df)
